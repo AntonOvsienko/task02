5c60394df332620156c17e1c8f04076704380b49
Anton <anton.sokira@gmail.com>
2024-01-16 22:52:15 +0200

    ver 1

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,28 +3,49 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syndicate.deployment.annotations.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
-        roleName = "hello_world-role",
-        isPublishVersion = true,
-        aliasName = "${lambdas_alias_name}"
+		roleName = "hello_world-role",
+		isPublishVersion = true,
+		aliasName = "${lambdas_alias_name}"
 )
-public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, Map<String, Object>> {
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    public Map<String, Object> handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-        if ("GET".equalsIgnoreCase(request.getHttpMethod()) && "/hello".equals(request.getPath())) {
-            Map<String, Object> resultMap = new HashMap<>();
-            resultMap.put("statusCode", 200);
-            resultMap.put("message", "Hello from Lambda");
-            return resultMap;
-        }
-        Map<String, Object> errorResult = new HashMap<>();
-        errorResult.put("statusCode", 400);
-        errorResult.put("message", "Bad Request");
-        return errorResult;
-    }
+	private static final int SC_OK = 200;
+	private static final int SC_BAD_REQUEST = 400;
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
+		try {
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_OK)
+					.withBody(objectMapper.writeValueAsString(resultMap));
+		} catch (IllegalArgumentException exception) {
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_BAD_REQUEST)
+					.withBody("Bad request");
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
